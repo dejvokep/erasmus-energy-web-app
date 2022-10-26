049b031aa72df4813d133f087a04f87cc03d5757
IAFB420 <geoke.jakub@gmail.com>
2022-10-26 21:04:40 +0200

    Database edit

--- components/power-station-info-box.js
+++ components/power-station-info-box.js
@@ -4,7 +4,7 @@ import PowerStationDetails from "./power-station-details";
 export default function PowerStationInfoBox({station}) {
     return <div className={styles.container}>
         <div className={styles.box}>
-            {station && <div className={styles.location}><h1>{station.name}</h1></div>}
+            {station && <div className={styles.location}><h1><a href={station.link}>{station.name}</a></h1></div>}
             {station && <PowerStationDetails name="Type" value={station.type}/>}
             {station && <PowerStationDetails name="Power output" value={station.power + " MW"}/>}
             {station && station.since && <PowerStationDetails name="Since" value={station.since}/>}
