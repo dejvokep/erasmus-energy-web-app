d73b8536ac6ee2f98da17370b26f9a646fb797fa
dejvokep <davidcubesvkdev@gmail.com>
2023-04-20 20:01:36 +0200

    Finished tasks page.

--- pages/tasks.js
+++ pages/tasks.js
@@ -32,24 +32,25 @@ const LIGHTING_TYPES = [
 
 ]
 
+const FORM_CLASSROOMS = {
+    name: "Nº Classrooms",
+    type: "number",
+    value: data => data.classrooms,
+    setValue: (modifyData, value) => modifyData(data => data.classrooms = value)
+}
+
 const TASKS = [
     {
         name: "Turn off lights when the classroom is empty - during lunch, planning periods, etc.",
         form: [
             {
-                name: "Nº Classrooms",
-                type: "number",
-                value: data => data.classrooms,
-                setValue: (modifyData, value) => modifyData(data => data.classrooms = value)
-            },
-            {
-                name: "Nº lights per classroom",
+                name: "Nº Lights per classroom",
                 type: "number",
                 value: data => data.lighting.lights,
                 setValue: (modifyData, value) => modifyData(data => data.lighting.lights = value)
             },
             {
-                name: "Nº hours a day saved",
+                name: "Nº Hours a day saved",
                 type: "number",
                 value: data => data.lighting.reduced,
                 setValue: (modifyData, value) => modifyData(data => data.lighting.reduced = value)
@@ -60,11 +61,85 @@ const TASKS = [
                 options: LIGHTING_TYPES,
                 value: data => data.lighting.type,
                 setValue: (modifyData, value) => modifyData(data => data.lighting.type = value)
+            },
+            FORM_CLASSROOMS
+        ],
+        calc: data => LIGHTING_TYPES.find(el => el.id === data.lighting.type).coefficient * +data.lighting.lights * +data.lighting.reduced * 0.000655292 * 180 * 0.00045359237 * +data.classrooms
+    },
+    {
+        name: "Start a walk/bike to school campaign",
+        form: [
+            {
+                name: "Nº Students that switched",
+                type: "number",
+                value: data => data.biking.switched,
+                setValue: (modifyData, value) => modifyData(data => data.biking.switched = value)
+            },
+            {
+                name: "Average distance to school (km)",
+                type: "number",
+                value: data => data.biking.distance,
+                setValue: (modifyData, value) => modifyData(data => data.biking.distance = value)
             }
         ],
-        calc: data => {
-            return LIGHTING_TYPES.find(el => el.id === data.lighting.type).coefficient * +data.lighting.lights * +data.lighting.reduced * 0.000655292 * 180 * 0.00045359237
-        }
+        calc: data => +data.biking.switched * +data.biking.distance * 0.621371192 / 25.29 * 2 * 19.4395602545263 * 180 * 0.00045359237
+    },
+    {
+        name: "Bring a reusable water bottle to school instead of buying plastic water bottles",
+        form: [
+            {
+                name: "Nº Bottles bought before",
+                type: "number",
+                value: data => data.bottles.before,
+                setValue: (modifyData, value) => modifyData(data => data.bottles.before = value)
+            },
+            {
+                name: "Nº Bottles bought after",
+                type: "number",
+                value: data => data.bottles.after,
+                setValue: (modifyData, value) => modifyData(data => data.bottles.after = value)
+            },
+            FORM_CLASSROOMS
+        ],
+        calc: data => (+data.bottles.before - +data.bottles.after) * 0.1091288385 * 36 * 0.00045359237 * +data.classrooms
+    },
+    {
+        name: "Use recycled paper instead of virgin paper",
+        form: [
+            {
+                name: "Nº Pages printed weekly",
+                type: "number",
+                value: data => data.paper.printed,
+                setValue: (modifyData, value) => modifyData(data => data.paper.printed = value)
+            },
+            FORM_CLASSROOMS
+        ],
+        calc: data => +data.paper.printed / 500 * 5.5 * 3.805 * 36 * 0.00045359237 * +data.classrooms
+    },
+    {
+        name: "Plant native trees",
+        form: [
+            {
+                name: "Nº Native trees planted",
+                type: "number",
+                value: data => data.trees.planted,
+                setValue: (modifyData, value) => modifyData(data => data.trees.planted = value)
+            },
+            FORM_CLASSROOMS
+        ],
+        calc: data => +data.trees.planted * 0.06
+    },
+    {
+        name: "Install motion sensing light switches in hallways, bathrooms, etc.",
+        form: [
+            {
+                name: "School's daily electricity consumption (kWh)",
+                type: "number",
+                value: data => data.sensors.consumption,
+                setValue: (modifyData, value) => modifyData(data => data.sensors.consumption = value)
+            }
+        ],
+        calc: data => +data.sensors.consumption * 0.1 * 0.655292 * 180 * 0.00045359237
     }
 ]
 
